34348937c8df68cf25668f2eae4621b02fe4eb36
Heitor Brasiel <81968639+heitorbrs@users.noreply.github.com>
2021-05-06 23:15:46 -0300

    Create Televisao.java

---
+++ exercicio4/Televisao.java
@@ -0,0 +1,21 @@
+public class Televisao {
+
+    private int canal=1; 
+    private int volume=0;
+
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+    
+    public int getCanal() {
+        return canal;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+}
